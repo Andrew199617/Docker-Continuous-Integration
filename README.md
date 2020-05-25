1f0c1ba0eb7521952f74d0f6dfe678be4c1573c0
Andrew Velez <andrewmauro199617@yahoo.com>
2020-05-25 11:29:34 -0700

    Added encryption so release.env and dev.env and .env will not be in plain text on server.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ENV Files
 - Add release.env for env variables to use with the release container.
 - Add dev.env for the env variables to sue with the dev container.
+
+# Args
+- -l is used to execute locally without pulling images.
+  - local will also force containers to be rebuilt
+- -p is used to pull images when the program is run in local mode.
+- -key is used to specify encryption key used to decrypt env files.
+  - You don' want to have plain text secrets on your server in case its hacked.
+- -iv is used for decrypting.
--- src/LoadEnv.js
+++ src/LoadEnv.js
@@ -1,9 +1,37 @@
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
+
+let iv = null;
+let key = null;
+for(let i = 2; i < process.argv.length; ++i) {
+  if(process.argv[i].includes('-key')) {
+    key = process.argv[i + 1];
+  }
+  else if(process.argv[i].includes('-iv')) {
+    iv = process.argv[i + 1];
+  }
+}
+
+if(!key || !iv) {
+  console.log('Iv or Key is missing!');
+  process.exit(1);
+}
+
+function decrypt(str) {
+  // const cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
+  // let encrypted = cipher.update(str, 'utf-8', 'hex');
+  // encrypted += cipher.final('hex');
+  // console.log('Encrypted:', encrypted);
+
+  const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv)
+  return decipher.update(str, 'hex', 'utf-8');
+}
 
 const localPath = path.join(process.cwd(), '.env');
 let localEnv = fs.readFileSync(localPath);
 localEnv = localEnv.toString();
+localEnv = decrypt(localEnv);
 localEnv = localEnv.split(/\n|\r\n/g);
 
 loop:
@@ -27,6 +55,7 @@ const DevEnvVariables = [];
 const devPath = path.join(process.cwd(), 'dev.env');
 let devEnv = fs.readFileSync(devPath);
 devEnv = devEnv.toString();
+devEnv = decrypt(devEnv);
 devEnv = devEnv.split(/\n|\r\n/g);
 
 const ReleaseEnvVariables = [];
@@ -34,6 +63,7 @@ const ReleaseEnvVariables = [];
 const releasePath = path.join(process.cwd(), 'release.env');
 let releaseEnv = fs.readFileSync(releasePath);
 releaseEnv = releaseEnv.toString();
+releaseEnv = decrypt(releaseEnv);
 releaseEnv = releaseEnv.split(/\n|\r\n/g);
 
 function ParseEnvFile(envFile, array) {
@@ -57,7 +87,6 @@ function ParseEnvFile(envFile, array) {
 
 ParseEnvFile(devEnv, DevEnvVariables);
 ParseEnvFile(releaseEnv, ReleaseEnvVariables);
-
 
 module.exports = {
   DevEnvVariables,
