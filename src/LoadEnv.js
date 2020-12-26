77ece986845cd97c23b485068f0ed1a1d6e7b0dd
Andrew Velez <andrewmauro199617@yahoo.com>
2020-12-26 14:30:03 -0500

    fix loglevel.

--- src/LoadEnv.js
+++ src/LoadEnv.js
@@ -5,7 +5,7 @@ const LogLevels = require('./LogLevels');
 
 let iv = null;
 let key = null;
-process.env.shouldLog = LogLevels.DEBUG;
+process.env.logLevel = LogLevels.DEBUG;
 for(let i = 2; i < process.argv.length; ++i) {
   if(process.argv[i].includes('-key')) {
     key = process.argv[i + 1];
